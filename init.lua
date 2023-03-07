bcf811381690a9723fe24a764d4023a43e45a1ba
kalthit <ragingkalthit@gmail.com>
2023-03-07 05:35:40 -0800

    upload init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+local function ensure(user, plugin)
+  local path = vim.fn.stdpath "data" .. "/lazy/" .. plugin
+  if not vim.loop.fs_stat(path) then
+    vim.notify("Downloading " .. plugin .. "...", vim.log.levels.INFO)
+    vim.fn.system {
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "--single-branch",
+      string.format("https://github.com/%s/%s.git", user, plugin),
+      path,
+    }
+  end
+  vim.opt.runtimepath:prepend(path)
+end
+
+ensure("folke", "lazy.nvim")
+require("core")

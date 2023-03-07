76b7541eae0ed4226d5313863d5ed5f0e002f0ce
kalthit <ragingkalthit@gmail.com>
2023-03-07 07:22:08 -0800

    chore: add colorscheme rose-pine

--- lua/modules.lua
+++ lua/modules.lua
@@ -1,6 +1,7 @@
 local lazy = require("lazy")
 lazy.setup({
   "rebelot/kanagawa.nvim",
+  { 'rose-pine/neovim', name = 'rose-pine' },
   "nvim-treesitter/nvim-treesitter",
   "neovim/nvim-lspconfig",
   "williamboman/mason.nvim",
